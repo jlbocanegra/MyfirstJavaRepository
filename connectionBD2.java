03077d92c928fa83949e7e1a56daff9ad30d1027
jlbocanegra <jlbocanegra@gmail.com>
2016-10-03 23:41:08 +0200

    Update java2

--- connectionBD2.java
+++ connectionBD2.java
@@ -34,7 +34,7 @@ public class connectionBD {
 			
 		}
 		catch (Exception e) {
-			System.out.println("***ATENCION***"+e.getMessage());
+			System.out.println("***ATENCION----PROBANDO***"+e.getMessage());
 		}
 
 	}
